9df8d97568a8e82067b492dc3a1685288b6111cc
faisalsaeed <faisalsaeed128@gmail.com>
2022-11-16 13:23:40 +0500

    pricing and why upmonth page

--- js/custom.js
+++ js/custom.js
@@ -26,9 +26,26 @@ $(function SieveOfEratosthenesCached(n, cache) {
         /*testimonials slider */
           var swiper = new Swiper(".client-logos", {
             spaceBetween: 10,
-            slidesPerView: 6,
-            freeMode: true,
+            freeMode: false,
             watchSlidesProgress: true,
+            breakpoints: {
+              640: {
+                slidesPerView: 2,
+                spaceBetween: 10,
+              },
+              768: {
+                slidesPerView: 3,
+                spaceBetween: 5,
+              },
+              1024: {
+                slidesPerView: 4,
+                spaceBetween: 10,
+              },
+              1920: {
+                slidesPerView: 6,
+                spaceBetween: 10,
+              },
+            },
           });
           var swiper2 = new Swiper(".client-info-wrap", {
             spaceBetween: 10,
@@ -45,10 +62,26 @@ $(function SieveOfEratosthenesCached(n, cache) {
         /*fast and simple */
         var swiper = new Swiper(".fast-simple-carousel", {
             spaceBetween: 0,
-            slidesPerView: 2,
             loop:false,
             mousewheel: true,
             centeredSlides: true,
+
+            breakpoints: {
+              640: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+                centeredSlides: false,
+              },
+              768: {
+                slidesPerView: 3,
+                spaceBetween: 5,
+              },
+              1024: {
+                slidesPerView: 4,
+                spaceBetween: 10,
+              },
+            },
+
             pagination: {
                 el: ".pagination-b",
                 type: "progressbar",
@@ -63,7 +96,9 @@ $(function SieveOfEratosthenesCached(n, cache) {
         /*secure and effecient */
         var swiper = new Swiper(".secure-effecient-carousel", {
             spaceBetween: 30,
+            slidesPerView: 1,
             mousewheel: true,
+            centeredSlides: false,
             pagination: {
                 el: ".pagination-c",
                 type: "progressbar",
@@ -73,6 +108,22 @@ $(function SieveOfEratosthenesCached(n, cache) {
                 prevEl: ".swiper-button-prev",
             },
         });
+
+        /*pricing sliding elements */
+        var swiper = new Swiper(".pricing-sliding-elements", {
+          spaceBetween: 30,
+          slidesPerView: 1,
+          mousewheel: false,
+          centeredSlides: false,
+          pagination: {
+              el: ".pagination-d",
+              type: "progressbar",
+          },
+          navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+          },
+      });
 
 
 
@@ -96,11 +147,12 @@ $(function SieveOfEratosthenesCached(n, cache) {
         /*add class in header*/
         $(window).on("scroll", function() {
             if ($(window).scrollTop() >= 1700) {
-                $("header").addClass("header-scroll ");
+                $("header").addClass("header-primary-scroll");
             } else {
-                $("header").removeClass("header-scroll ");
+                $("header").removeClass("header-primary-scroll");
             }
         });
+
 
 
     });
